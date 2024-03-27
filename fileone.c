bfbfad7cca922c4645f1ddd9fc39fa3d0d5cfbc4
danimohli <danimohlife@gmail.com>
2024-03-27 13:08:16 +0100

    monty one

---
+++ fileone.c
@@ -0,0 +1,86 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element onto the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	char *arg = strtok(NULL, " \n"); /* Get the argument from command line*/
+
+	if (arg == NULL || !is_numeric(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = atoi(arg);
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+
+	(void)line_number;
+}
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
