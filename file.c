0a7aebd67c2e7572029c4f02fed35c2641db314d
danimohli <danimohlife@gmail.com>
2024-03-27 13:33:30 +0100

    nop implement

--- file.c
+++ file.c
@@ -17,3 +17,15 @@ int is_numeric(const char *str)
 	}
 	return (1);
 }
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	/* This function does nothing */
+}
