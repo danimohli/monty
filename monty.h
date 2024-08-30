0ed85196cbfad6f35c2d541f2dab397602dbaf6a
danimohli <danimohlife@gmail.com>
2024-08-30 12:30:46 +0100

    head

--- monty.h
+++ monty.h
@@ -1,14 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-
+#include <ctype.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/types.h>
 
-
-extern int DATA; /* Global Variable Defination*/
+extern int DATA; /* Global Variable Definition*/
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -40,14 +40,15 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
-void nop(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
-void look_up(char *opcode, unsigned int line_number, stack_t *stack);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
-#endif
+void look_up(char *opcode, unsigned int line_number, stack_t **stack);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif /* MONTY_H */
