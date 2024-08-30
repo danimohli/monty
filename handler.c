1f9d0cbbb72c31820b1cb94ca09e2c00fb2fc174
danimohli <danimohlife@gmail.com>
2024-08-30 05:42:00 +0100

    handler

---
+++ handler.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * look_up - call-up function
+ * @opcode: opcode to lookup
+ * @line_number: argument to pass to function
+ * @stack: the stack
+ */
+void look_up(char *opcode, unsigned int line_number, stack_t *stack)
+{
+	int x;
+
+	instruction_t array[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"NULL", NULL}
+	};
+
+	for (x = 0; array[x].opcode != NULL; x++)
+	{
+		if (strcmp(opcode, array[i].opcode) == 0)
+		{
+			array[i].f(stack, line_number);
+			return;
+		}
+	}
+
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
