08f66f70325339f3a39ec41f2cab18657657a6ed
danimohli <danimohlife@gmail.com>
2024-08-30 15:17:01 +0100

    error_output

--- stack_func_two.c
+++ stack_func_two.c
@@ -74,7 +74,7 @@ void t_div(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
